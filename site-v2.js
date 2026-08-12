b7bbb05c31b5e03faa52ebe16fdf408decc5323b
NAM5AN <133747621+NAM5AN@users.noreply.github.com>
2026-08-12 11:19:22 +0900

    chore: load final Featured and Video preview overrides

--- site-v2.js
+++ site-v2.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const V='20260812ah';
+  const V='20260812ai';
 
   const faviconSvg='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect width="64" height="64" rx="15" fill="#101113"/><path d="M17 16v32M17 32l25-16M17 32l27 16" fill="none" stroke="#f9faf8" stroke-width="6" stroke-linecap="square"/></svg>';
   document.querySelectorAll('link[rel~="icon"]').forEach(node=>node.remove());
@@ -48,6 +48,7 @@
   [
     'https://i.ytimg.com/vi/jINNCqnUSL8/maxresdefault.jpg',
     'https://i.ytimg.com/vi/dI_J-0qeb5A/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/TPrPnkTMmTo/maxresdefault.jpg',
     'https://i.ytimg.com/vi/RRaPlueNU8Y/maxresdefault.jpg',
     photoPreview,
     'https://drive.google.com/thumbnail?id=1IMS882egUEvxo32byMvxnYUVQnDglh0x&sz=w1600',
@@ -141,6 +142,7 @@
       './preview-controller.js',
       './photo-preview-concept.js',
       './design-preview-duo.js',
+      './featured-preview-nbn.js',
       './video-preview-nbn.js',
       './view-state.js'
     ];
